92adcedda4464cbd8f0bdc841a234d524f3564a6
Anton Loukianov <anton.loukianov@gmail.com>
2010-10-29 19:09:56 -0700

    fft_test now does convolution both in frequency and space. There is a difference

--- fft_test.m
+++ fft_test.m
@@ -1,6 +1,6 @@
 close all;
 % Load image from file
-img = imread('fft_test.bmp');
+img = imread('fft_circle.bmp');
 img = rgb2gray(img);
 % Show image
 imshow(img);
@@ -10,15 +10,26 @@ kern.setColor('r');
 
 kimg = imcrop(img, kern.getPosition);
 dat = normxcorr2(kimg, img);
-figure, imshow(dat), colorbar;
+dat2 = conv2(img, rot90(kimg,2));
+h1 = figure('Name', 'Normalized, unthresholded'), imshow(dat), colorbar;
 
-s = regionprops(dat > 0.2, dat, 'basic');
-hold on;
+mval = max(dat2(:));
+dat2 = dat2./mval; % Normalize
+h2 = figure('Name', 'Spacial Correlation'), imshow(dat2), colorbar;
+
+s = regionprops(dat > 0.7, dat, 'basic');
+s2 = regionprops(dat2 > 0.7, dat2, 'basic');
+
 cm = s.Centroid;
-plot(cm(1), cm(2), 'r*');
+cm2 = s2.Centroid;
+figure(2), hold on, plot(cm(1), cm(2), '*r');
+figure(3), hold on, plot(cm2(1), cm2(2), '*g');
 
 pos = kern.getPosition;
 cm = cm - pos(3:4)./2;
-figure(1), hold on, plot(cm(1), cm(2), 'r*');
+cm2 = cm2 - pos(3:4)./2;
+figure(1), hold on, scatter(cm(1), cm(2), 'r');
+figure(1), hold on, scatter(cm2(1), cm2(2), 'g');
 cm
+
 
